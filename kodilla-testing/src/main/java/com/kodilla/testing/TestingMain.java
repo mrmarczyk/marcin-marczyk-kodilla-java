5672502f9cb43880e08fb01a404d6d96c6253378
mrmarczyk <mrmarczyk@gmail.com>
2017-07-16 21:06:26 +0200

    Moduł 6.2 first  commit

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -3,8 +3,28 @@ package com.kodilla.testing;
 /**
  * Created by marcinmarczyk on 16.07.2017.
  */
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        Calculator calculator = new Calculator();
+        int addResult = calculator.add(3, 1);
+        int subResult = calculator.subtract(2, 2);
+        if (addResult == 4) && (subResult == 0) {
+            System.out.println("Calculator test OK");
+        } else {
+            System.out.println("Calculator has error!");
+        }
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/user/SimpleUser.java
@@ -0,0 +1,19 @@
+package com.kodilla.testing.user;
+
+/**
+ * Created by marcinmarczyk on 16.07.2017.
+ */
+public class SimpleUser {
+
+    private String username;
+
+    public SimpleUser(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+}
+
+
